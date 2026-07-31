3a9537371cf6d8c8ffc10ad0c6cd0edb9b984ddf
Maxim <metallmuff@gmail.com>
2026-07-31 14:35:59 +1200

    KS-3-2: исправление замечаний

--- src/Main.kt
+++ src/Main.kt
@@ -3,8 +3,8 @@ fun main() {
     val midleName = "Сергеевна"
     var lastName = "Андреева"
     var age = 20
-    println("$lastName $firstName $midleName , $age")
+    println("[$lastName $firstName $midleName, $age]")
     lastName = "Сидорова"
     age = 22
-    println("$lastName $firstName $midleName , $age")
+    println("[$lastName $firstName $midleName, $age]")
 }
