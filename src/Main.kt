b7aa5ee13254d055dc472395f149a0c5f6b8c793
Maxim <metallmuff@gmail.com>
2026-08-01 13:39:02 +1200

    KS-3-3 Таблица умножения исправленная

--- src/Main.kt
+++ src/Main.kt
@@ -2,7 +2,12 @@ fun main() {
     val num = 3
     var text = ""
     for (i in 1..9) {
-        text += "$num x $i = ${num * i}\n"
+        if (text.isEmpty()) {
+            text += "$num x $i = ${num * i}"
+        } else {
+            text += "\n$num x $i = ${num * i}"
+        }
+
     }
     println(text)
 }
