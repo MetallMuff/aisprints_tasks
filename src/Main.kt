b4c59f2316a387b4fc7faab96461c9e73a226961
Maxim <metallmuff@gmail.com>
2026-08-01 20:46:05 +1200

    KS-3-5 парсер сервера

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,9 @@
-fun main(){
-    var from = "E2"
-    var to = "E4"
-    var moveNumber = "1"
-    println("$from-$to;$moveNumber")
-    from = "D2"
-    to = "D3"
-    moveNumber = "2"
-    println("$from-$to;$moveNumber")
-
-
+fun main() {
+    val way = "D2-D4;0"
+    val from = way.split("-")
+    println(from[0])
+    val to = way.split("-", ";")
+    println(to[1])
+    val moveNum = way.split("-", ";")
+    println(moveNum[2])
 }
