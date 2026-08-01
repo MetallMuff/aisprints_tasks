e357ea5c1377f6f868b86fe4fabb278f028e6ca3
Maxim <metallmuff@gmail.com>
2026-08-01 12:07:44 +1200

    KS-3-3 Таблица умножения

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,7 @@
-fun main(){
-    val name = "Максим"
-    var greeting = "Добрый день $name!"
-    println(greeting)
-    greeting = "Добрый вечер $name!"
-    println(greeting)
+fun main() {
+    val num = 3
+    for (i in 1..9) {
+        val result = num * i
+        println("$num × $i = ${result.toString().padStart(2)}")
+    }
 }
