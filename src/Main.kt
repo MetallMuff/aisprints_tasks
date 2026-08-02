a85d068eed4f24c4bb4c47b1cfa6bbc542184996
Maxim <metallmuff@gmail.com>
2026-08-03 09:35:56 +1200

    KS-4-2 таможенный терминал

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,12 @@
-const val NUMBER_OF_TABLES = 13
-
+const val START_CONTROL_WEIGHT = 35
+const val END_CONTROL_WEIGHT = 100
+const val CONTROL_VOLUME = 100
 fun main() {
-    val todayBookedTables = 13
-    val tomorrowBookedTables = 9
-    val isAvailableToday = todayBookedTables < NUMBER_OF_TABLES
-    val isAvailableTomorrow = tomorrowBookedTables < NUMBER_OF_TABLES
-    println("Доступность столиков на сегодня: $isAvailableToday\nДоступность столиков на завтра: $isAvailableTomorrow")
-
+    val category = "'Average'"
+    val firstWeight = 20 // вес первого груза
+    val firstVolume = 80 // объем первого груза
+    val secondWeight = 50 // вес второго груза
+    val secondVolume = 100 // объем второго груза
+    println("Груз с весом $firstWeight кг и объемом $firstVolume л соответствует категории $category: ${firstWeight > START_CONTROL_WEIGHT && firstWeight <= END_CONTROL_WEIGHT && firstVolume < CONTROL_VOLUME}")
+    println("Груз с весом $secondWeight кг и объемом $secondVolume л соответствует категории $category: ${secondWeight > START_CONTROL_WEIGHT && secondWeight <= END_CONTROL_WEIGHT && secondVolume < CONTROL_VOLUME}")
 }
