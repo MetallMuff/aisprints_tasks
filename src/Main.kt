4f9f7de4e47cc6815f61ffdb132c88094543069f
Maxim <metallmuff@gmail.com>
2026-08-03 11:42:08 +1200

    KS-4-3 мониторинг растений

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,14 @@
 const val HUMIDITY = 20
 const val MIN_HUMIDITY_FOR_GROWTH = 20
 const val SEASON = "winter"
-const val WINTER = "winter"
+
 
 fun main() {
     val isSunny = true // погода сегодня : солнечно
     val isTentOpen = true // тент: открыт
-    val isNotWinter = SEASON != WINTER
-    val hasEnoughHumidity = HUMIDITY >= MIN_HUMIDITY_FOR_GROWTH
+    val winter = "winter"
+    val isNotWinter = SEASON != winter
+    val hasEnoughHumidity = HUMIDITY == MIN_HUMIDITY_FOR_GROWTH
     println(
         "Благоприятные ли условия сейчас для роста бобовых?" +
                 " ${isSunny && isTentOpen && hasEnoughHumidity && isNotWinter}"
