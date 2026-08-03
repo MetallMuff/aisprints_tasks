42fe849be824525e22792224f90ab36221343a08
Maxim <metallmuff@gmail.com>
2026-08-04 09:38:58 +1200

    KS-4-4 программа тренировок

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,13 @@
-const val HUMIDITY = 22
-const val MIN_HUMIDITY_FOR_GROWTH = 20
-const val SEASON = "summer"
-const val UNSUITABLE_SEASON = "winter"
-
 fun main() {
-    val isSunny = true // погода сегодня : солнечно
-    val isTentOpen = true // тент: открыт
-    val isNotWinter = SEASON != UNSUITABLE_SEASON
-    val hasEnoughHumidity = HUMIDITY >= MIN_HUMIDITY_FOR_GROWTH
-    val isGoodConditions = isSunny && isTentOpen && hasEnoughHumidity && isNotWinter
-    if (isGoodConditions) {
-        println("Благоприятные условия для посадки!")
-    } else {
-        println("Неблагоприятные условия для посадки!")
-    }
+    val day = 5
+    val isHandsAndPress = day % 2 == 1
+    val isLegsAndBack = day % 2 == 0
+    println(
+        "Упражнения для рук:".padEnd(25) + "$isHandsAndPress\n" +
+        "Упражнения для ног:".padEnd(25) + "$isLegsAndBack\n" +
+        "Упражнения для спины:".padEnd(25) + "$isLegsAndBack\n" +
+        "Упражнения для пресса:".padEnd(25) + "$isHandsAndPress"
+    )
+
 
 }
