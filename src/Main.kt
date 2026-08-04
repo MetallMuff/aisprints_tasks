87196d2817af0f76f526c9fcbe82e314c5033402
Maxim <metallmuff@gmail.com>
2026-08-04 13:47:02 +1200

    KS-4-5 отплытие корабля fix

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,6 @@
 fun main() {
     println("Есть ли повреждение корпуса? (Да/Нет)")
-    val damage = readln().equals("да", ignoreCase = true)
+    val hasDamage = readln().equals("да", ignoreCase = true)
     println("Введите размер экипажа")
     val crew = readln().toInt()
     println("Введите количество ящиков провизии")
@@ -8,16 +8,15 @@ fun main() {
     println("Введите погодные условия. (Хорошие / Плохие)")
     val isGoodWeather = readln().equals("хорошие", ignoreCase = true)
     println()
-    val isNoDamage = !damage
-    val hasEnoughCrew = crew in 55..70
+    val isNoDamage = !hasDamage
+    val hasEnoughCrew = crew >= 50 && crew <= 70
     val hasEnoughProvision = provision > 50
-    val fullConditions = isNoDamage && hasEnoughCrew && hasEnoughProvision
-    val minimumConditions = !isNoDamage && crew == 70 && provision >= 50 && isGoodWeather
-    if (fullConditions) {
-        println("Все условия соблюдены. Пароход готов к выходу.")
-    } else if (minimumConditions) {
-        println("Минимальные условия соблюдены. Пароход готов к выходу.")
+    val canDepart = (isNoDamage && hasEnoughCrew && hasEnoughProvision) ||
+            (!isNoDamage && crew == 70 && provision >= 50 && isGoodWeather)
+    if (canDepart) {
+        println("Судно может выйти в рейс")
     } else {
-        println("Условия не соблюдены. Выход запрещен!")
+        println("Судно не может выйти в рейс")
     }
+
 }
